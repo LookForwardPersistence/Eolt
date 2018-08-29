c8b460d01771b5393a7deca973bbcee5942e8425
LookForwardPersistence <fqhua_5@sina.cn>
2018-08-29 23:40:53 +0800

    fix bug

--- js/main.js
+++ js/main.js
@@ -179,9 +179,7 @@ app = new Vue({
                 limit: 10,
                 table: 'result'
             }).then((data) => {
-                alert(JSON.stringify(data))
                 var result = data.data.rows[0].roll_number;
-                alert(result)
             this.bet_result = result;
 
             var rate_100 = 25;
