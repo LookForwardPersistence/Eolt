e381cceecf96d017667d907527114cdf1d1bb085
LookForwardPersistence <fqhua_5@sina.cn>
2018-09-01 06:45:11 +0800

    t

--- js/main.js
+++ js/main.js
@@ -103,9 +103,11 @@ app = new Vue({
 
             happyeosslot_true_balance = happyeosslot_true_balance.data.rows[0].deposit.balance.split(' ', 1)[0];
             this.eop = happyeosslot_balance / (happyeosslot_true_balance - 1250);
+            var efr = happyeosslot_balance / (happyeosslot_true_balance - 1250)
+            alert(efr)
             var nums= happyeosslot_true_balance.data.rows[0].supply.split(' ')
             var num = nums[0];//happyeosslot_true_balance.data.rows[0].supply.split(' ', 1)[0];
-            this.current_price = (num/25000)*0.1*this.eop;
+            this.current_price = (num/25000)*0.1*efr;
             //this.eop = new Number(this.eop).toFixed(4);
             return this.eop;
         },
